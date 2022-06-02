1aa2687ccf1638066df18842237f68a330f28eb1
yyzdtccjdtc <yyang11@wpi.edu>
2022-06-02 12:19:50 -0700

    update resize to new version

--- examples/resize.rs
+++ examples/resize.rs
@@ -6,6 +6,7 @@ use resize::Type::Triangle;
 use std::env;
 use std::fs::File;
 use std::io;
+use rgb::FromSlice;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -29,7 +30,7 @@ fn main() {
     let (w1, h1) = (decoder.get_width(), decoder.get_height());
     let dst_dims: Vec<_> = args[2].split("x").map(|s| s.parse().unwrap()).collect();
     let (w2, h2) = (dst_dims[0], dst_dims[1]);
-    let mut resizer = resize::new(w1, h1, w2, h2, Gray8, Triangle);
+    // let mut resizer = resize::new(w1, h1, w2, h2, Gray8, Triangle);
     let mut dst = vec![0; w2 * h2];
 
     let mut outfh: Box<dyn io::Write> = if args[3] == "-" {
@@ -45,7 +46,7 @@ fn main() {
     loop {
         match decoder.read_frame() {
             Ok(frame) => {
-                resizer.resize(frame.get_y_plane(), &mut dst);
+                resize::new(w1, h1, w2, h2, Gray8, Triangle).unwrap().resize(frame.get_y_plane().as_gray(), dst.as_gray_mut());
                 let out_frame = y4m::Frame::new([&dst, &[], &[]], None);
                 if encoder.write_frame(&out_frame).is_err() {
                     break;
